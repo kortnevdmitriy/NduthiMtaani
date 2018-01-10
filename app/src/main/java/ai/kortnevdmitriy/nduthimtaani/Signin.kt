559b46e158c8c5515ca10cd0aa527f17da2a1b79
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2018-01-10 18:56:20 +0300

    Nduthi Mtaani - First commit

---
+++ app/src/main/java/ai/kortnevdmitriy/nduthimtaani/Signin.kt
@@ -0,0 +1,12 @@
+package ai.kortnevdmitriy.nduthimtaani
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
